716387a947dd1a7201cd02c15c93cd5d780cd072
shioujyelu <shioujyelu@digiwin.com>
2019-04-19 18:08:21 +0800

    update

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Application {
 
     @RequestMapping("/test")
-    public String home() {
+    public String test() {
         return "DockerService1";
     }
 
