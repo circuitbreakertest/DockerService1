4c52f9fbd9934275a80106f7d76fcbc34f68f7b6
shioujyelu <shioujyelu@digiwin.com>
2019-04-19 18:06:21 +0800

    update

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -14,6 +14,11 @@ public class Application {
         return "DockerService1";
     }
 
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
+    
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
